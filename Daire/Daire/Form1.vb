725b73efcf13ae879583a86238f77ce6764b5ad2
Mert Can Altın <37827216+mertcanaltin@users.noreply.github.com>
2018-04-09 20:50:06 +0300

    Add files via upload

---
+++ Daire/Daire/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+
+    Private Sub HesaplaButton_Click(sender As Object, e As EventArgs) Handles HesaplaButton.Click
+        Dim r, Alan As Decimal
+        r = rTextBox.Text
+        Alan = Math.PI * r ^ 2 'Alan = Math.PI * r *r
+        AlanTextBox.Text = Alan
+    End Sub
+End Class
